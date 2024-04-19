4552f9cd75ad899ff773926c5cb9ad8863e38441
implaymo <136201112+implaymo@users.noreply.github.com>
2024-04-19 16:36:55 +0100

    Fix game not starting. Change Xpath from start_game.

--- main.py
+++ main.py
@@ -18,6 +18,6 @@ accept_cookies = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="qc-c
 
 accept_cookies.click()
 
-start_game = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="main-frame-error"]/div[6]/canvas')))
+start_game = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="darkSwitch"]')))
 
 start_game.send_keys(Keys.SPACE).perform()

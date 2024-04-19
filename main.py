75f0c43efa9b39a45d21f3c37d8ba18966616083
implaymo <136201112+implaymo@users.noreply.github.com>
2024-04-19 16:18:57 +0100

    Manage to make game start

--- main.py
+++ main.py
@@ -17,3 +17,7 @@ wait = WebDriverWait(driver, 10)
 accept_cookies = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="qc-cmp2-ui"]/div[2]/div/button[2]')))
 
 accept_cookies.click()
+
+start_game = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="main-frame-error"]/div[6]/canvas')))
+
+start_game.send_keys(Keys.SPACE).perform()

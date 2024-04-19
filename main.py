d3d179d4c245d5e5cca449e70edcd2d70806d166
implaymo <136201112+implaymo@users.noreply.github.com>
2024-04-19 16:11:18 +0100

    Manage to click cookies.

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
 
 
 chrome_options = webdriver.ChromeOptions()
@@ -8,3 +12,8 @@ driver = webdriver.Chrome(options=chrome_options)
 driver.get("https://elgoog.im/dinosaur-game/")
 driver.maximize_window()
 wait = WebDriverWait(driver, 10)
+
+
+accept_cookies = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="qc-cmp2-ui"]/div[2]/div/button[2]')))
+
+accept_cookies.click()

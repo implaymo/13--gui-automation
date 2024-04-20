7ae214f4f4dfcdc5f70f5adf41e31ae023fff9ac
implaymo <136201112+implaymo@users.noreply.github.com>
2024-04-20 17:00:14 +0100

    Adjust size of screenshot. Now it is only screenshoting the location where objects can appear

--- new_approach.py
+++ new_approach.py
@@ -1,12 +1,16 @@
 import pyautogui 
 from PIL import ImageGrab
+import time
 
 
 def detect_objetcs(i): 
-    whole_screen = ImageGrab.grab(bbox=(0, 350, 1700, 850))
+    whole_screen = ImageGrab.grab(bbox=(150, 530, 1700, 900))
     whole_screen.save(f"screenshots/game_image{i}.jpg")
     whole_screen.close()
 
 pyautogui.hotkey('win', 'down')
 pyautogui.click()         
-pyautogui.press("space")         
+pyautogui.press("space")
+time.sleep(2)
+for i in range(10):
+    detect_objetcs(i)          

9ad6764f64b6f9a457afe20b0c0cb51b01f972fd
implaymo <136201112+implaymo@users.noreply.github.com>
2024-04-20 17:05:53 +0100

    Decrease amount of screenshot in X axis.

--- new_approach.py
+++ new_approach.py
@@ -3,8 +3,11 @@ from PIL import ImageGrab
 import time
 
 
+bird_to_tuck_y_position = 570
+other_obstacales_y_position = 630
+
 def detect_objetcs(i): 
-    whole_screen = ImageGrab.grab(bbox=(150, 530, 1700, 900))
+    whole_screen = ImageGrab.grab(bbox=(150, 530, 700, 900))
     whole_screen.save(f"screenshots/game_image{i}.jpg")
     whole_screen.close()
 

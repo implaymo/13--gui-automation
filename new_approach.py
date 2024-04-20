517ab77db5be3c81a71a395f47534b082b1c951b
implaymo <136201112+implaymo@users.noreply.github.com>
2024-04-20 16:43:11 +0100

    Manage to close vscode window to make the code work on my website game.

--- new_approach.py
+++ new_approach.py
@@ -1,12 +1,12 @@
-import pyautogui
+import pyautogui 
 from PIL import ImageGrab
 
 
-def detect_objetcs(i):
+def detect_objetcs(i): 
     whole_screen = ImageGrab.grab(bbox=(0, 350, 1700, 850))
     whole_screen.save(f"screenshots/game_image{i}.jpg")
     whole_screen.close()
 
-
+pyautogui.hotkey('win', 'down')
 pyautogui.click()         
 pyautogui.press("space")         

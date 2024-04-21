8389907c725c402baf31386a0dfc77b284eac0c6
implaymo <136201112+implaymo@users.noreply.github.com>
2024-04-21 19:42:05 +0100

    Manage to check properly if game is in dark theme or light theme.

--- new_approach.py
+++ new_approach.py
@@ -5,7 +5,17 @@ import time
 light_theme = False
 dark_theme = False
 
+def change_to_dark_theme():
+    global light_theme, dark_theme
+    light_theme = False
+    dark_theme = True
+    return dark_theme, light_theme
 
+def change_to_light_theme():
+    global light_theme, dark_theme
+    dark_theme = False
+    light_theme = True
+    return dark_theme, light_theme
 
 def screenshot(): 
     image = ImageGrab.grab().convert('L')
@@ -16,28 +26,23 @@ def screenshot():
     
 def check_background_theme(image_data):
     x = 0 
-    y = 0  
+    y = 300  
     color = image_data[x, y]
-    if color < 50:
-        dark_theme = True
-        return dark_theme
+    print(f"{color}")
+    if color < 200:
+        change_to_dark_theme()
     else:
-        light_theme = True
-        return light_theme
+        change_to_light_theme()
     
 def detect_object(image_data):
     if dark_theme: 
-        for x in range(200, 500):
-            for y in range(500, 800): 
+        for x in range(800,960):
+            for y in range(800, 850): 
                 object_color = image_data[x, y]               
-                if object_color > 170:
+                if object_color < 200:
                     print(f"WHITE OBJECT {object_color}") 
                     pyautogui.keyDown('up')
-    else:
-        for x in range(200, 500):
-            for y in range(300, 670):         
-                object_color = image_data[x, y]
-                if object_color < 70:                   
+                else:
                     print(f"BLACK OBJECT {object_color}")  
                     pyautogui.keyDown('up')
      

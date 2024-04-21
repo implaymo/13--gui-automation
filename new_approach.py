fb7556e62c79cd094e2bb3a8077cd5bb20d91942
implaymo <136201112+implaymo@users.noreply.github.com>
2024-04-21 09:54:18 +0100

    Manage to get the color of the background.

--- new_approach.py
+++ new_approach.py
@@ -1,19 +1,39 @@
 import pyautogui 
-from PIL import ImageGrab
+from PIL import ImageGrab, Image
+import os
 import time
 
+light_theme = False
+dark_theme = False
 
+
+screenshots_folder_path = "./screenshots"
 bird_to_tuck_y_position = 570
 other_obstacales_y_position = 630
 
-def detect_objetcs(i): 
-    whole_screen = ImageGrab.grab(bbox=(150, 530, 700, 900))
-    whole_screen.save(f"screenshots/game_image{i}.jpg")
-    whole_screen.close()
+def screenshot(i): 
+    image = ImageGrab.grab(bbox=(150, 530, 900, 900)).convert('L')
+    image.save(f"screenshots/game_image{i}.jpg")
+    image_data = image.load()
+    get_background_color(image_data)
+
+def get_background_color(image_data):
+    x = 0 
+    y = 0  
+    color = image_data[x, y]
+    print(color)
+    if color < 50:
+        dark_theme = True
+        print("DARK THEME")
+    else:
+        light_theme = True
+        print("LIGHT THEME")
 
 pyautogui.hotkey('win', 'down')
 pyautogui.click()         
 pyautogui.press("space")
 time.sleep(2)
 for i in range(10):
-    detect_objetcs(i)          
+    time.sleep(1)
+    screenshot(i)  
+    

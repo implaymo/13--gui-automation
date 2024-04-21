ccc65cd262905c80c4cc09fb1557d9245dbb0b24
implaymo <136201112+implaymo@users.noreply.github.com>
2024-04-21 13:00:02 +0100

    Got the screenshot showing the area were the obstacales can show.

--- new_approach.py
+++ new_approach.py
@@ -5,10 +5,6 @@ import time
 light_theme = False
 dark_theme = False
 
-
-bird_to_tuck_y_position = 570
-other_obstacales_y_position = 630
-
 def change_to_dark_theme():
     global light_theme, dark_theme
     light_theme = False
@@ -22,12 +18,13 @@ def change_to_light_theme():
     return dark_theme, light_theme
 
 def screenshot(): 
-    image = ImageGrab.grab(bbox=(150, 530, 900, 900)).convert('L')
+    image = ImageGrab.grab(bbox=(200, 600, 900, 870)).convert('L')
+    image.save("game_image.jpg")
     image_data = image.load()
-    check_theme(image_data)
+    check_background_theme(image_data)
     detect_object(image_data)
     
-def check_theme(image_data):
+def check_background_theme(image_data):
     x = 0 
     y = 0  
     color = image_data[x, y]
@@ -41,31 +38,30 @@ def check_theme(image_data):
     
 def detect_object(image_data):
     if dark_theme:
-        for x in range(200, 300):
-            for y in range(100, 300):
+        for x in range(250, 700):
+            for y in range(100, 265): 
                 object_color = image_data[x, y]               
-                if object_color < 170:
+                if object_color > 170:
                     print("DARK THEME")
                     print(x, y)
                     print(object_color) 
                     pyautogui.press("space")
-                    return
     if light_theme:
-        for x in range(250, 400):
-            for y in range(200, 350):
+        for x in range(250, 700):
+            for y in range(100, 265):      
                 object_color = image_data[x, y]        
                 if object_color < 70:
                     print("LIGHT THEME")
                     print(x, y)
                     print(object_color)   
                     pyautogui.press("space")
-                    return
   
 pyautogui.hotkey('win', 'down')
 pyautogui.click()         
 pyautogui.press("space")
 time.sleep(2)
 
-while True:
+game_on = True
+while game_on:
     screenshot()
     

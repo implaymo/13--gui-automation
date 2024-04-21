5e08e528ced8547604a5ec2bd91e00609edb3693
implaymo <136201112+implaymo@users.noreply.github.com>
2024-04-21 19:00:24 +0100

    Fix detect_object. Dark theme was checking black obstacales and white theme was checking white obstacales.

--- new_approach.py
+++ new_approach.py
@@ -5,20 +5,10 @@ import time
 light_theme = False
 dark_theme = False
 
-def change_to_dark_theme():
-    global light_theme, dark_theme
-    light_theme = False
-    dark_theme = True
-    return dark_theme, light_theme
 
-def change_to_light_theme():
-    global light_theme, dark_theme
-    dark_theme = False
-    light_theme = True
-    return dark_theme, light_theme
 
 def screenshot(): 
-    image = ImageGrab.grab(bbox=(200, 600, 1800, 870)).convert('L')
+    image = ImageGrab.grab().convert('L')
     image.save("game_image.jpg")
     image_data = image.load()
     check_background_theme(image_data)
@@ -29,39 +19,34 @@ def check_background_theme(image_data):
     y = 0  
     color = image_data[x, y]
     if color < 50:
-        change_to_dark_theme()
+        dark_theme = True
         return dark_theme
     else:
-        change_to_light_theme()
+        light_theme = True
         return light_theme
     
-    
 def detect_object(image_data):
-    if dark_theme:
-        for x in range(250, 700):
-            for y in range(100, 265): 
+    if dark_theme: 
+        for x in range(200, 500):
+            for y in range(500, 800): 
                 object_color = image_data[x, y]               
                 if object_color > 170:
-                    print("DARK THEME")
-                    print(x, y)
-                    print(object_color) 
-                    pyautogui.press("space")
-    if light_theme:
-        for x in range(250, 700):
-            for y in range(100, 265):      
-                object_color = image_data[x, y]        
-                if object_color < 70:
-                    print("LIGHT THEME")
-                    print(x, y)
-                    print(object_color)   
-                    pyautogui.press("space")
-  
+                    print(f"WHITE OBJECT {object_color}") 
+                    pyautogui.keyDown('up')
+    else:
+        for x in range(200, 500):
+            for y in range(300, 670):         
+                object_color = image_data[x, y]
+                if object_color < 70:                   
+                    print(f"BLACK OBJECT {object_color}")  
+                    pyautogui.keyDown('up')
+     
 pyautogui.hotkey('win', 'down')
-pyautogui.click()         
+pyautogui.click()             
 pyautogui.press("space")
 time.sleep(2)
 
 game_on = True
 while game_on:
     screenshot()
-    
+                                   

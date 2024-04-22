2488d73bbf401ad3f4247781e1b037b92f40ade5
implaymo <136201112+implaymo@users.noreply.github.com>
2024-04-22 10:56:02 +0100

    Fix game not working on dark_theme. Needs threshold fixing.

--- new_approach.py
+++ new_approach.py
@@ -4,8 +4,14 @@ import time
 
 light_theme = False
 dark_theme = False
+x_min = 240                          
+x_max = 350
+y_min_cac = 800
+y_max_cac = 830
+y_min_bird = 750
+y_max_bird = 780          
 
-def change_to_dark_theme():
+def change_to_dark_theme():       
     global light_theme, dark_theme
     light_theme = False
     dark_theme = True
@@ -16,6 +22,47 @@ def change_to_light_theme():
     dark_theme = False
     light_theme = True
     return dark_theme, light_theme
+    
+def check_background_theme(image_data):
+    x = 0 
+    y = 300  
+    color = image_data[x, y]
+    if color < 150:
+        change_to_dark_theme()
+    else:
+        change_to_light_theme()
+              
+def detect_object_cactus(image_data):
+    if dark_theme:
+        for x in range(x_min, x_max):
+            for y in range(y_min_cac, y_max_cac):  
+                object_color = image_data[x, y]
+                if object_color > 100:
+                    pyautogui.press("space")
+                    return
+    elif light_theme:
+        for x in range(x_min, x_max):
+            for y in range(y_min_cac, y_max_cac):  
+                object_color = image_data[x, y]
+                if object_color < 99:
+                    pyautogui.press("space")
+                    return
+                    
+def detect_object_birds(image_data):
+    if dark_theme:
+         for x in range(x_min, x_max):
+            for y in range(y_min_bird, y_max_bird):           
+                object_color = image_data[x, y]               
+                if object_color > 100:
+                    pyautogui.press("space")
+                    return
+    elif light_theme:
+        for x in range(x_min, x_max):              
+            for y in range(y_min_bird, y_max_bird):           
+                object_color = image_data[x, y]        
+                if object_color < 99:                   
+                    pyautogui.press("space")
+                    return                        
 
 def screenshot(): 
     image = ImageGrab.grab().convert('L')
@@ -24,47 +71,7 @@ def screenshot():
     detect_object_cactus(image_data)
     detect_object_birds(image_data)
     
-def check_background_theme(image_data):
-    x = 0 
-    y = 300  
-    color = image_data[x, y]
-    if color < 200:
-        change_to_dark_theme()
-    else:
-        change_to_light_theme()
-              
-def detect_object_cactus(image_data):
-    if dark_theme:
-        for x in range(340, 380):
-                for y in range(800, 830):  
-                    object_color = image_data[x, y]               
-                    if object_color > 200:
-                        pyautogui.press("space")
-                        return
-    if light_theme:
-        for x in range(340, 380):
-                for y in range(800, 830):          
-                    object_color = image_data[x, y]        
-                    if object_color < 200:                   
-                        pyautogui.press("space")
-                        return
-def detect_object_birds(image_data):
-    if dark_theme:
-        for x in range(250, 380):
-                for y in range(750, 780):           
-                    object_color = image_data[x, y]               
-                    if object_color > 200:
-                        pyautogui.press("space")
-                        return
-    if light_theme:
-        for x in range(250, 380):
-                for y in range(750, 780):           
-                    object_color = image_data[x, y]        
-                    if object_color < 200:                   
-                        pyautogui.press("space")
-                        return                        
-                    
-                    
+             
 pyautogui.hotkey('win', 'down')
 pyautogui.click()             
 pyautogui.press("space")

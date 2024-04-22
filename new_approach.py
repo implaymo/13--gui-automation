8d7e9b622fcae5f220f44bfed3d33d6e1e28c093
implaymo <136201112+implaymo@users.noreply.github.com>
2024-04-22 19:10:14 +0100

    Manage to reach 1420 highscore with this x and y values. Needs improvemenet when switching to dark mode. It jumps automatically without any obstacale.

--- new_approach.py
+++ new_approach.py
@@ -4,12 +4,19 @@ import time
 
 light_theme = False
 dark_theme = False
-x_min = 360                          
-x_max = 400
-y_min_cac = 800
-y_max_cac = 830
-y_min_bird = 750
-y_max_bird = 780          
+x_min_cac = 380                          
+x_max_cac = 400
+y_min_cac = 770
+y_max_cac = 800
+
+x_min_bird = 400                          
+x_max_bird = 410
+y_min_bird = 730
+y_max_bird = 735  
+ 
+sleep_time = time.sleep(0)
+color_to_jump_dark = 100
+color_to_jump_light = 90       
 
 def change_to_dark_theme():       
     global light_theme, dark_theme
@@ -24,7 +31,7 @@ def change_to_light_theme():
     return dark_theme, light_theme
     
 def check_background_theme(image_data):
-    x = 0 
+    x = 299 
     y = 300  
     color = image_data[x, y]
     if color < 150:
@@ -34,33 +41,33 @@ def check_background_theme(image_data):
               
 def detect_object_cactus(image_data):
     if dark_theme:
-        for x in range(x_min, x_max):
+        for x in range(x_min_cac, x_max_cac):
             for y in range(y_min_cac, y_max_cac):  
                 object_color = image_data[x, y]
-                if object_color > 100:
+                if object_color > color_to_jump_dark:
                     pyautogui.press("space")
                     return
     elif light_theme:
-        for x in range(x_min, x_max):
+        for x in range(x_min_cac, x_max_cac):
             for y in range(y_min_cac, y_max_cac):  
                 object_color = image_data[x, y]
-                if object_color < 99:
+                if object_color < color_to_jump_light:
                     pyautogui.press("space")
                     return
                     
 def detect_object_birds(image_data):
     if dark_theme:
-         for x in range(x_min, x_max):
+         for x in range(x_min_bird, x_max_bird):
             for y in range(y_min_bird, y_max_bird):           
                 object_color = image_data[x, y]               
-                if object_color > 100:
+                if object_color > color_to_jump_dark:
                     pyautogui.press("space")
                     return
     elif light_theme:
-        for x in range(x_min, x_max):              
+        for x in range(x_min_bird, x_max_bird):              
             for y in range(y_min_bird, y_max_bird):           
                 object_color = image_data[x, y]        
-                if object_color < 99:                   
+                if object_color < color_to_jump_light:                   
                     pyautogui.press("space")
                     return                        
 

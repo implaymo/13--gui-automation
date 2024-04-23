3a61c661e39b00a888b611d466b87cd0975a1bb4
implaymo <136201112+implaymo@users.noreply.github.com>
2024-04-23 13:38:31 +0100

    Add function 'change_jump_time_cactus' to change the timing of jump when the speed increases. Better performance in the first 700 points. Needs fixing when changing to dark theme.

--- new_approach.py
+++ new_approach.py
@@ -2,21 +2,22 @@ import pyautogui
 from PIL import ImageGrab
 import time
 
+count = 0
 light_theme = False
 dark_theme = False
-x_min_cac = 380                          
+x_min_cac = 310                        
 x_max_cac = 400
-y_min_cac = 770
-y_max_cac = 800
+y_min_cac = 780
+y_max_cac = 810
 
-x_min_bird = 400                          
-x_max_bird = 410
+x_min_bird = 420                          
+x_max_bird = 500
 y_min_bird = 730
 y_max_bird = 735  
  
 sleep_time = time.sleep(0)
-color_to_jump_dark = 100
-color_to_jump_light = 90       
+color_to_jump_dark = 90
+color_to_jump_light = 84      
 
 def change_to_dark_theme():       
     global light_theme, dark_theme
@@ -31,15 +32,27 @@ def change_to_light_theme():
     return dark_theme, light_theme
     
 def check_background_theme(image_data):
-    x = 299 
-    y = 300  
+    x = 1780
+    y = 600  
     color = image_data[x, y]
-    if color < 150:
+    if color <= 90:
         change_to_dark_theme()
     else:
         change_to_light_theme()
+        
+def change_jump_timing_cactus():
+    global count, x_min_cac, x_max_cac
+    if count > 7:
+        x_min_cac = 370                        
+        x_max_cac = 450
+    elif count > 50:
+        x_min_cac = 430
+        x_max_cac = 550              
+    return x_min_cac, x_max_cac
               
 def detect_object_cactus(image_data):
+    global count
+    change_jump_timing_cactus()
     if dark_theme:
         for x in range(x_min_cac, x_max_cac):
             for y in range(y_min_cac, y_max_cac):  
@@ -53,15 +66,19 @@ def detect_object_cactus(image_data):
                 object_color = image_data[x, y]
                 if object_color < color_to_jump_light:
                     pyautogui.press("space")
+                    count += 1
                     return
                     
 def detect_object_birds(image_data):
+    global count
+    change_jump_timing_cactus()
     if dark_theme:
          for x in range(x_min_bird, x_max_bird):
             for y in range(y_min_bird, y_max_bird):           
                 object_color = image_data[x, y]               
                 if object_color > color_to_jump_dark:
                     pyautogui.press("space")
+                    count += 1
                     return
     elif light_theme:
         for x in range(x_min_bird, x_max_bird):              
@@ -69,6 +86,7 @@ def detect_object_birds(image_data):
                 object_color = image_data[x, y]        
                 if object_color < color_to_jump_light:                   
                     pyautogui.press("space")
+                    count += 1
                     return                        
 
 def screenshot(): 
